4573c563502611b68629ca35c7e4eb942f8cb65d
JeanBaptisteR <robert.jeanbaptiste@laposte.net>
2017-02-20 14:29:31 +0100

    Manipulation de Parcelable

--- app/src/main/java/fr/android/androidexercises/Book.java
+++ app/src/main/java/fr/android/androidexercises/Book.java
@@ -1,6 +1,9 @@
 package fr.android.androidexercises;
 
-public class Book {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Book implements Parcelable {
 
     private String name;
     private String author;
@@ -9,6 +12,34 @@ public class Book {
         this.name = name;
         this.author = author;
     }
+
+    protected Book(Parcel in) {
+        name = in.readString();
+        author = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(author);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
 
     public String getName() {
         return name;
