f3889150a7a34267a5a88d11ddcdc9ff43ca7a71
JeanBaptisteR <robert.jeanbaptiste@laposte.net>
2017-02-21 10:07:52 +0100

    Utilisation de Retrofit

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -3,6 +3,13 @@ package fr.android.androidexercises;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import timber.log.Timber;
 
 public class LibraryActivity extends AppCompatActivity {
@@ -14,18 +21,22 @@ public class LibraryActivity extends AppCompatActivity {
 
         // Plant logger cf. Android Timber
         Timber.plant(new Timber.DebugTree());
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://henri-potier.xebia.fr/").addConverterFactory(GsonConverterFactory.create()).build();
+        HenriPotierService service = retrofit.create(HenriPotierService.class);
+        service.listBooks().enqueue(new Callback<List<Book>>() {
+            @Override
+            public void onResponse(Call<List<Book>> call, Response<List<Book>> response) {
+                for(Book book : response.body()) {
+                    Timber.i("Book name : %s ", book.getTitle());
+                }
+            }
 
-        // TODO build Retrofit
+            @Override
+            public void onFailure(Call<List<Book>> call, Throwable t) {
+                Timber.e(t.getMessage());
+            }
+        });
 
-        // TODO create a service
-
-        // TODO listBooks()
-
-        // TODO enqueue call and display book title
-
-        // TODO log books
-
-        // TODO display book as a list
     }
 
 }
